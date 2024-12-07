dd6ccd2fd41fc1d8d6d6e6a7119e97aa752054d7
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-08 05:13:38 +0600

    home page modified

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -156,7 +156,7 @@ const Home = () => {
             </div>
 
             {/* Footer */}
-            <footer className="footer p-10 bg-base-200 text-base-content">
+            {/* <footer className="footer p-10 bg-base-200 text-base-content">
                 <div>
                     <span className="footer-title">Services</span> 
                     <a className="link link-hover">Meal Booking</a>
@@ -175,7 +175,7 @@ const Home = () => {
                     <a className="link link-hover">Privacy policy</a>
                     <a className="link link-hover">Cookie policy</a>
                 </div>
-            </footer>
+            </footer> */}
         </div>
     );
 };
